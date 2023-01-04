af33925c7cb982307261fbcc45eb1f24b792514f
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2023-01-04 14:28:11 +0200

    Delete Exams directory

--- Exams/src/June2020/parking/Main.java
+++
@@ -1,7 +0,0 @@
-package parking;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- Exams/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        double num = 0;
-        System.out.println(num * -1);
-    }
-}
