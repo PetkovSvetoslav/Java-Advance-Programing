2d206b91530f618b930cd9553ac653fc2ef63a36
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2023-01-04 14:30:45 +0200

    Delete EXAMS directory

--- EXAMS/OLD EXAM/LAST EXAM/fairyShop/models/Happy.java
+++
@@ -1,9 +0,0 @@
-package fairyShop.models;
-
-public class Happy extends BaseHelper{
-    private static final int BASE_ENERGY = 100;
-
-    public Happy(String name) {
-        super(name, BASE_ENERGY);
-    }
-}
